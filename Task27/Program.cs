22411f65e4808bd8fea1bb339ceb6744bcd9b880
Mammaeva <110192855+Mammaeva@users.noreply.github.com>
2022-08-17 01:03:59 +0300

    Add files via upload

---
+++ Task27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+while (n > 0)
+{
+    summa = summa + n % 10;
+    n /= 10;
+}
+Console.Write("Сумма: " + summa);
