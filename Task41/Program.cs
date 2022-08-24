b1bf594b759b3240ec55892bab8a00b1ce0125f4
Mammaeva <110192855+Mammaeva@users.noreply.github.com>
2022-08-24 11:16:44 +0300

    Add files via upload

---
+++ Task41/Program.cs
@@ -0,0 +1,22 @@
+﻿void Task41 ()
+{
+    Console.WriteLine("Сколько чисел хотите ввести:  ");
+    int M = Convert.ToInt32(Console.ReadLine());
+   int [] array = new int [M];
+   int count = 0;
+
+    Console.WriteLine("введите числа:  ");
+
+   for (int i = 0; i < M; i++)
+   {
+       array[i]= Convert.ToInt32(Console.ReadLine()); 
+       if(array[i]> 0) count ++;
+    
+   }
+        Console.WriteLine();
+        Console.WriteLine($"Пользователь ввел {count} положительных чисел");
+}
+
+Task41();
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,20 @@
+﻿void Task43()
+{
+    Console.WriteLine("Введите значение b1: ");
+    int b1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите значение k1: ");
+    int k1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите значение b2: ");
+    int b2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите значение k2: ");
+    int k2 = Convert.ToInt32(Console.ReadLine());
+    if(k1==k2) Console.WriteLine("Прямые параллельны");
+    else 
+    {
+        double x= (b2 - b1) / (k1 - k2);
+        double y  = k2 * x + b2;
+        Console.WriteLine($"Точка пересечения двух прямых ({Math.Round(x,2)} ; {Math.Round(y,2)})");
+    }
+
+}
+Task43();
