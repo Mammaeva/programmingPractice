39a9eed9fab799b742b91da13c40c7c71ccdc8ff
Mammaeva <110192855+Mammaeva@users.noreply.github.com>
2022-08-31 00:20:01 +0300

    Add files via upload

---
+++ Task_54/Program.cs
@@ -0,0 +1,53 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void GetSequence(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int x = 0; x < matr.GetLength(1) - 1; x++)
+            {
+                if (matr[i, x] < matr[i, x + 1])
+                {
+                    int num = matr[i, x + 1];
+                    matr[i, x + 1] = matr[i, x];
+                    matr[i, x] = num;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = new int[4, 4];
+
+FillArray(matrix);
+Console.WriteLine();
+
+PrintArray(matrix);
+Console.WriteLine("    >>>>");
+
+GetSequence(matrix);
+
+PrintArray(matrix);
