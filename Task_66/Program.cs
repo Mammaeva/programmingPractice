04d45e9e03bb13cad52d382f3328924d3555fa4b
Mammaeva <110192855+Mammaeva@users.noreply.github.com>
2022-09-02 21:06:06 +0300

    Add files via upload

---
+++ Task_66/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int SumNumbers(int m, int n)
+{
+    if (m == n) return n;
+    else return m + SumNumbers(m += 1, n);
+}
+
+Console.WriteLine
+($"Сумма чисел в промежутке от {m} до {n}: {SumNumbers(m, n)}");
---
+++ Task_68/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+static int FunAckermann(int a, int b)
+{
+    if (a == 0)
+    {
+        return b + 1;
+    }
+    else if (b == 0)
+    {
+        return FunAckermann(a - 1, 1);
+    }
+    else
+    {
+        return FunAckermann(a - 1, FunAckermann(a, b - 1));
+    }
+}
+
+Console.WriteLine("A(m,n)=" + FunAckermann(m, n));
